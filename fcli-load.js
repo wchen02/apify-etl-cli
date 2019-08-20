1dfc3b5eb06531c863515856ea76bd048b300681
Wensheng Chen <hi@wenshengchen.com>
2019-08-20 01:25:17 -0700

    feat: Add load CLI command

---
+++ fcli-load.js
@@ -0,0 +1,53 @@
+const log = require('loglevel');
+const program = require('commander');
+const apify = require('apify-etl-lib')();
+
+const defaultOptions = {
+    silent: false,
+    debug: false,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydatabase',
+    normalizedDataDir: 'normalized',
+};
+
+program
+    .description('Retrieves last run dataset items')
+    .option('-s, --silent', 'Set log level to warn', defaultOptions.silent)
+    .option('-d, --debug', 'Set log level to debug', defaultOptions.debug)
+    .option('-h, --host <host>', 'Set database host', defaultOptions.host)
+    .option('-u, --user <user>', 'Set database user', defaultOptions.user)
+    .option('-p, --password <password>', 'Set database password', defaultOptions.password)
+    .option('-D, --database <database>', 'Set database name', defaultOptions.database)
+    .option('--normalized-data-dir <normalizedDataDir>', 'Set the normalized data directory to archive', defaultOptions.normalizedDataDir)
+    .parse(process.argv)
+
+async function main() {
+    log.setLevel(getLogLevel());
+
+    const options = {
+        logLevel: getLogLevel(),
+        db: {
+            host: program.host,
+            user: program.user,
+            password: program.password,
+            database: program.database,
+        },
+        dataDir: program.normalizedDataDir,
+    };
+
+    await apify.load(options);
+}
+
+function getLogLevel() {
+    if (program.silent) {
+        return log.levels.WARN;
+    } else if (program.debug) {
+        return log.levels.DEBUG;
+    } else {
+        return log.levels.INFO;
+    }
+}
+
+main();
--- fcli.js
+++ fcli.js
@@ -3,5 +3,6 @@ const program = require('commander');
 program
   .version('0.0.1')
   .command('normalize', 'Normalizes dataset items')
+  .command('load', 'Load normalized dataset items into database')
   .command('archive', 'Archives data and downloaded files')
   .parse(process.argv);
